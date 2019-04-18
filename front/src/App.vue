da1ad2923617fb3f7af0ee8ae37ee22940c1c5eb
ivanblinov2k17 <ivanblinov2015@mail.ru>
2019-04-18 19:11:53 +0300

    added working annotator

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-annotator
+      @move-end="annoChanged"
+      @resize-end="annoChanged"
+      :minSize="[50, 50]"
+      :grid="[1, 1]"
+    >
+      <img draggable="false" src="./assets/logo.png">
+
+      <rect slot="annotation" stroke="green" x="0" y="10" width="170" height="100"/>
+      <polygon slot="annotation" stroke="purple" x="0" y="10" width="100" height="100"/>
+
+      <rect slot="drawing" stroke="red"/>
+    </v-annotator>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VAnnotator from "vue-annotator";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    VAnnotator
+  },
+  methods: {
+    annoChanged(e) {
+      let attr = e.node.attributes;
+      console.log(attr.x, attr.y, attr.width, attr.height);
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+rect,
+polygon {
+  fill-opacity: 0;
 }
 </style>
