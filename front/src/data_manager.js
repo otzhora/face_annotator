1ac9cb8fc252e045dcdc8dd302bf5e803e11202c
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-05-03 00:29:40 +0300

    no more logging

--- front/src/data_manager.js
+++ front/src/data_manager.js
@@ -8,52 +8,34 @@ class Manager {
   }
 
   async load_annotations() {
-    console.log("loading annotations...");
-    await axios
-      .get(`${this.url}images`)
-      .then(res => {
-        this.data = Object.assign({}, res["data"]);
-        this.ids = Object.keys(this.data);
-        console.log("loading finished");
-      })
-      .catch(console.log);
+    await axios.get(`${this.url}images`).then(res => {
+      this.data = Object.assign({}, res["data"]);
+      this.ids = Object.keys(this.data);
+    });
   }
 
   async get_faces(id_) {
     if (!(id_ in this.data)) {
-      console.log(`id ${id_} not in data`);
       return [];
     }
     let faces = [];
-    console.log(`loading faces for ${id_}`);
 
-    await axios
-      .get(`${this.url}images/${id_}/annotations`)
-      .then(res => {
-        console.log(`finished loading faces for ${id_}`);
-        faces = res["data"];
-      })
-      .catch(console.log);
+    await axios.get(`${this.url}images/${id_}/annotations`).then(res => {
+      faces = res["data"];
+    });
     return faces;
   }
 
   async update_faces(id_, faces) {
     if (!(id_ in this.data)) {
-      console.log(`id ${id_} not in data`);
       return [];
     }
-    console.log(`sending faces for ${id_}`);
     const options = {
       headers: { "content-type": "application/json" }
     };
     const data = JSON.stringify({ faces });
 
-    await axios
-      .post(`${this.url}images/${id_}/annotations`, data, options)
-      .then(() => {
-        console.log(`finished sending faces for ${id_}`);
-      })
-      .catch(console.log);
+    await axios.post(`${this.url}images/${id_}/annotations`, data, options);
 
     this.data[id_]["faces"] = faces;
   }
@@ -62,4 +44,4 @@ class Manager {
   }
 }
 
-module.exports = Manager;
+export default Manager;
