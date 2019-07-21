cfad7b36c2ffa605d35e7cf8e85a44fed9cdbd28
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-07-22 00:15:47 +0400

    support for cmd line arguments and better error handling

--- api/fill_database.js
+++ api/fill_database.js
@@ -1,29 +1,36 @@
 const MongoClient = require("mongodb").MongoClient;
 const fs = require("fs");
-const path = require("path");
+
+const argv = process.argv.slice(2);
+
+const user_name = argv[0] || "dbAnnotationUser";
+const user_password = argv[1] || "dbAnnotationPassword";
+const cluster_location = argv[2] || "cluster0-zmdg3.gcp.mongodb.net";
+const url = `mongodb+srv://${user_name}:${user_password}@${cluster_location}/test?retryWrites=true&w=majority`;
 
 const ann_path = "./data/faces_test.json";
-const uri =
-  "mongodb+srv://dbFaceAnnotations:MongodbPassword@faceannotatordb-bupak.gcp.mongodb.net/test?retryWrites=true";
-const collection_name = "FaceAnnotationsCODebug";
-const db_name = "FaceAnnotatorDB";
+const db_name = "pm-anniversary";
+const collection_name = "faceAnnotationsDebug";
 
-let data = JSON.parse(fs.readFileSync(ann_path));
-
-let subm = [];
+const data = JSON.parse(fs.readFileSync(ann_path));
+const subm = [];
 
 for (let key in data) {
-  subm.push({ path: key, faces: data[key] });
+  subm.push({ url: key, faces: data[key] });
 }
 
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const client = new MongoClient(url, { useNewUrlParser: true });
 
-(async () => {
-  await client.connect();
-  await client
-    .db(db_name)
-    .collection(collection_name)
-    .insertMany(subm);
-
-  await client.close();
-})();
+client
+  .connect()
+  .then(client => {
+    return client
+      .db(db_name)
+      .collection(collection_name)
+      .insertMany(subm);
+  })
+  .then(result => {
+    console.log("Insertion result: \n", result);
+    client.close();
+  })
+  .catch(console.log);
