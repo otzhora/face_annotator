5fcc3b78cb074fb00aed05120e74d45f96825600
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-07-23 19:15:37 +0400

    refactored get_urls() to get_url(_id)

--- api/api.js
+++ api/api.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const multer = require("multer");
 const upload = multer();
 const app = express();
+const port = 5001;
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,30 +14,32 @@ app.use(express.static("data"));
 const Manager = require("./data_manager");
 const manager = new Manager(true);
 
-// load annotations
-(async () => {
-  await manager.load_annotations();
-  manager.gen_absolute_pathes();
-})();
+manager.load_annotations().then(() => {
+  app.listen(port, () => console.log(`Listening on port ${port}`));
+});
 
 app.get("/images", (req, res) => {
-  console.log("handling get images request");
-  res.send(manager.get_data());
+  manager.get_data().then(data => {
+    res.send(data);
+  });
 });
 
 app.get("/images/:_id/annotations", (req, res) => {
-  console.log("handling get images/id/annotations request");
-  res.send(manager.get_faces(req.params["_id"]));
+  manager.get_faces(req.params["_id"]).then(face_array => {
+    res.send(face_array);
+  });
+});
+
+app.get("/images/:_id", (req, res) => {
+  manager.get_url(req.params["_id"]).then(face_array => {
+    res.send(face_array);
+  });
 });
 
 app.post("/images/:_id/annotations", upload.array(), (req, res) => {
-  console.log("handling post images/id/annotations request");
   let faces = req.body["faces"];
   manager
     .update_faces(req.params._id, faces)
     .then(() => res.send("Oki"))
     .catch(console.log);
 });
-
-const port = 5001;
-app.listen(port, () => console.log(`Listening on port ${port}`));
