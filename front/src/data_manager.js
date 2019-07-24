bb1e3e5ba90f0151b79f0542866799790e4fd68f
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-07-24 04:19:29 +0400

    refactored frontend data_manager logic, so that now we dont store copy of a database on a client side

--- front/src/data_manager.js
+++ front/src/data_manager.js
@@ -1,46 +1,39 @@
 const axios = require("axios");
 
 class Manager {
-  constructor() {
-    this.url = "http://localhost:5001/";
-    this.data = {};
-    this.ids = [];
+  constructor(url = "http://localhost:5000/") {
+    this.url = url;
   }
 
-  async load_annotations() {
-    await axios.get(`${this.url}images`).then(res => {
-      this.data = Object.assign({}, res["data"]);
-      this.ids = Object.keys(this.data);
+  load_id_list() {
+    return axios.get(`${this.url}images/id_list`).then(res => {
+      return res["data"];
     });
   }
 
-  async get_faces(id_) {
-    if (!(id_ in this.data)) {
-      return [];
-    }
-    let faces = [];
-
-    await axios.get(`${this.url}images/${id_}/annotations`).then(res => {
-      faces = res["data"];
+  get_faces(id_) {
+    return axios.get(`${this.url}images/${id_}/annotations`).then(res => {
+      return res["data"];
     });
-    return faces;
   }
 
-  async update_faces(id_, faces) {
-    if (!(id_ in this.data)) {
-      return [];
-    }
+  update_faces(id_, faces) {
     const options = {
       headers: { "content-type": "application/json" }
     };
     const data = JSON.stringify({ faces });
 
-    await axios.post(`${this.url}images/${id_}/annotations`, data, options);
-
-    this.data[id_]["faces"] = faces;
+    return axios
+      .post(`${this.url}images/${id_}/annotations`, data, options)
+      .then(res => {
+        return res["data"];
+      });
   }
-  get_data() {
-    return Object.assign({}, this.data);
+
+  get_photo_info(id_) {
+    return axios.get(`${this.url}images/${id_}`).then(res => {
+      return res["data"];
+    });
   }
 }
 
