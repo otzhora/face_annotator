7f674b0b3125dd116d97e11f0afd7b949504474a
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-08-29 20:55:17 +0300

    displaying annotations

--- front/src/App.vue
+++ front/src/App.vue
@@ -10,8 +10,16 @@
     >
       <img draggable="false" :src="this.photo" />
 
-      <rect slot="annotation" stroke="green" x="0" y="10" width="170" height="100" />
-      <polygon slot="annotation" stroke="purple" x="0" y="10" width="100" height="100" />
+      <rect
+        v-for="(item,index) in annotations"
+        :key="index"
+        slot="annotation"
+        stroke="red"
+        :x="item.x"
+        :y="item.y"
+        :width="item.width"
+        :height="item.height"
+      />
 
       <rect slot="drawing" stroke="red" />
     </v-annotator>
@@ -26,6 +34,7 @@ export default {
   data() {
     return {
       photo: "",
+      annotations: [],
       manager: null,
       ids: [],
       url: "http://localhost:5001",
@@ -43,8 +52,21 @@ export default {
     async fetchData() {
       this.loading = true;
       this.ids = await this.manager.load_id_list();
+
       let photo_rel_url = await this.manager.get_url(this.ids[0]);
       this.photo = `${this.url}/${photo_rel_url}`;
+
+      this.annotations = await this.manager.get_faces(this.ids[0]); // TODO: make get_url and get_faces requests at the same time
+      for (let item of this.annotations) {
+        let name = Object.keys(item)[0];
+        let anno = item[name];
+        item.name = name;
+        item.x = anno[3];
+        item.y = anno[0];
+        item.width = anno[1] - anno[3];
+        item.height = anno[2] - anno[0];
+      }
+
       this.loading = false;
     }
   },
