0133831311c752fc378a575f457262eef5ea5344
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-08-29 20:33:20 +0300

    loading photos from server

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,26 +1,37 @@
 <template>
   <div id="app">
+    <div v-if="loading">Loading...</div>
     <v-annotator
       @move-end="annoChanged"
       @resize-end="annoChanged"
       :minSize="[50, 50]"
       :grid="[1, 1]"
+      v-if="!loading"
     >
-      <img draggable="false" src="./assets/logo.png">
+      <img draggable="false" :src="this.photo" />
 
-      <rect slot="annotation" stroke="green" x="0" y="10" width="170" height="100"/>
-      <polygon slot="annotation" stroke="purple" x="0" y="10" width="100" height="100"/>
+      <rect slot="annotation" stroke="green" x="0" y="10" width="170" height="100" />
+      <polygon slot="annotation" stroke="purple" x="0" y="10" width="100" height="100" />
 
-      <rect slot="drawing" stroke="red"/>
+      <rect slot="drawing" stroke="red" />
     </v-annotator>
   </div>
 </template>
 
 <script>
 import VAnnotator from "vue-annotator";
-
+import Manager from "./data_manager";
 export default {
   name: "app",
+  data() {
+    return {
+      photo: "",
+      manager: null,
+      ids: [],
+      url: "http://localhost:5001",
+      loading: false
+    };
+  },
   components: {
     VAnnotator
   },
@@ -28,7 +39,18 @@ export default {
     annoChanged(e) {
       let attr = e.node.attributes;
       console.log(attr.x, attr.y, attr.width, attr.height);
+    },
+    async fetchData() {
+      this.loading = true;
+      this.ids = await this.manager.load_id_list();
+      let photo_rel_url = await this.manager.get_url(this.ids[0]);
+      this.photo = `${this.url}/${photo_rel_url}`;
+      this.loading = false;
     }
+  },
+  created() {
+    this.manager = new Manager(this.url);
+    this.fetchData();
   }
 };
 </script>
