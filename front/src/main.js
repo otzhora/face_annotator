8afdec39cf0f4f2b1d7e0446a7cb7639be496901
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-09-21 17:39:14 +0300

    vuetify

--- front/src/main.js
+++ front/src/main.js
@@ -1,11 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store/store";
-import vuetify from './plugins/vuetify';
+import vuetify from "./plugins/vuetify";
+
+import "@babel/polyfill";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@mdi/font/css/materialdesignicons.css";
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
   vuetify,
+  render: h => h(App),
   store
 }).$mount("#app");
--- front/src/plugins/vuetify.js
+++ front/src/plugins/vuetify.js
@@ -1,10 +1,16 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import ru from "vuetify/es5/locale/ru";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-  icons: {
-    iconfont: 'mdi',
+  lang: {
+    locales: { ru },
+    current: "ru"
   },
+  icons: {
+    iconfont: "mdi"
+  }
 });
