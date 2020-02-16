60c6c842432feb49c766c8bf0ae3bfbddcf53636
Yuriy Rogachev <y_rogachev@inbox.ru>
2020-02-16 20:10:14 +0300

    added scroll area to image selector

--- frontend/src/components/ImageSelector.vue
+++ frontend/src/components/ImageSelector.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="container d-flex justify-md-space-between">
-    <div class="image_container" v-for="(id, index) in id_list" :key="index">
-      <img :src="photo_url(id)" @click="selected(id)" />
+  <vue-custom-scrollbar class="scroll-area">
+    <div class="container d-flex justify-md-space-between">
+      <div class="image_container" v-for="(id, index) in id_list" :key="index">
+        <img :src="photo_url(id)" @click="selected(id)" />
+      </div>
     </div>
-  </div>
+  </vue-custom-scrollbar>
 </template>
 
 <script>
+import vueCustomScrollbar from "vue-custom-scrollbar";
+
 export default {
   name: "ImageSelector",
+  components: { vueCustomScrollbar },
   computed: {
     id_list: function() {
       return this.$store.state.id_list;
@@ -33,5 +38,13 @@ export default {
 
 img {
   max-height: 100%;
+  padding-right: 50px;
+}
+
+.scroll-area {
+  position: relative;
+  margin: auto;
+  width: 100%;
+  height: 25vh;
 }
 </style>
